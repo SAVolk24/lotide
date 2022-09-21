83baaa7da841ce08aff9de7239e16668596a812d
Stephen Volk <savolk24@gmail.com>
2022-09-21 14:02:20 -0400

    Added code for letterPositions

---
+++ letterPosition.js
@@ -0,0 +1,32 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  eqArrays(actual, expected) === true
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const letterPositions = function (sentence) {
+  //write code here
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
